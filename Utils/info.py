a29d94d7b08ea2ca9e639e52ac92b1a38b4c3f24
AndyMore <morteyandrews@gmail.com>
2024-10-14 22:29:05 +0100

    App Setup

---
+++ Utils/info.py
@@ -0,0 +1,25 @@
+column_1 = """
+### Attrition Predictor
+Attrition Predictor is a Machine Learning application that  predicts the likelihood of an employee leaving a company based on various demographic and job-related factors.
+
+### Key Features
+- **View Data:** Access proprietory data.
+- **Dashboard:** Explore interactive data visualizations for insights.
+- **Real-time Prediction:** Instantly see predictions for employee attrition.
+- **History:** See past predictions made.
+
+### User Benefits
+- **Data-driven Decisions:** Make informed decisions backed by data analytics.
+- **Easy Machine Learning:** Utilize powerful machine learning algorithms effortlessly.
+
+"""
+
+column_2 = """
+### Machine Learning Integration
+- **Model Selection:** Choose between two advanced models for accurate predictions.
+- **Seamless Integration:** Integrate predictions into your workflow with a user-friendly interface.
+- **Probability Estimates:** Gain insights into the likelihood of predicted outcomes.
+
+### Need Help?
+For collaborations contact me at [morteyandrews@gmail.com](mailto:morteyandrews@gmail.com).
+"""
